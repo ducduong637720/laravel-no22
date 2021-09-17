d0e1c12a9f7052abe7a4fcfe704852ab98442011
ducduong637720 <ducxauzaifa2k@gmail.com>
2021-09-17 16:42:27 +0700

    make it better

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('backend/dashboard');
+    return view('backend/posts/list');
 })->name('home');
 
 Route::get('/posts/show', function () {
