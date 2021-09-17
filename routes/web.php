190255e9c23da672923c17bdf1de7bb3ce3ca976
ducduong637720 <ducxauzaifa2k@gmail.com>
2021-09-17 17:11:37 +0700

    Sửa bài tập buổi 4

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('backend/posts/list');
+    return view('welcome');
 })->name('home');
 
 Route::get('/posts/show', function () {
